3453f8f4cd15d2cfd2f8ae5cc7925d92b871d9b8
Herelitick <geg_gge_geg@mail.ru>
2022-03-15 19:49:48 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Component} from "./components/Component"
+
+function App() {
+  return (
+    <div className="App">
+        <Component></Component>
+        <div>А ЭТО ОБЫЧНЫЙ БЛОК HTML</div>
+    </div>
+  );
+}
+
+export default App;
