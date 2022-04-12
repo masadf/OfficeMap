bb106e068f3b1da045ab47f704ba804e8e62fea4
Администратор <pevik@outlook.com>
2022-04-12 13:13:39 +0300

    server venv

--- Back/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('\main')
-def start():
-    return "Server started! (or not, I don't know)"
-
-if __name__ == '__main__':
-    app.run(debug=True)
